27824a6718c7066d070cea4337bf01732a25b63a
John Doe <john@sample.com>
2019-11-22 17:42:35 +0530

    update on 11/22

--- src/test/java/com/test/rough/Oracon.java
+++ src/test/java/com/test/rough/Oracon.java
@@ -5,67 +5,6 @@ import java.util.HashMap;
 import java.sql.Connection;
 
 public class Oracon {
-    public static HashMap<String, Object> param1 = new HashMap <String,Object>();
-
-    public static void main(String[] args) throws Exception {
-        //@Hostanme:Portname
-       // http://www.java2s.com/Code/Jar/j/Downloadjdbcoraclejar.htm
-        param1.put("HOST","jdbc:oracle:thin:@manishqa.manish.com:1521");
-        param1.put("USERNAME","mrajurka");
-        param1.put("PASSWORD","password");
-        param1.put("QUERY","SELECT * FROM table Where Column_name = 'value'");
-        param1.put("COLUMN NAME","customer_name");
-        param1.put("EXPECTED RESULT","HP MeasureWare Interface");
-         Oracon  obj1  = new Oracon();
-         obj1.querySQL(param1);
-
-        String response_value = obj1.querySQL(param1);
-        System.out.println(response_value);
-
-      //  String response_node  =ts1.getJsonNodeValue(params);
-
-//SELECT * FROM ( SELECT id,NAME,VERSION_STATUS, TO_DATE('19700101','yyyymmdd') + (MODIFIED_DATE/24/60/60)MAXDATE, ROW_NUMBER() OVER (PARTITION BY NAME ORDER BY MODIFIED_DATE DESC) RNK FROM aradmin.INT_PIM_Product where NAME = '${SmartNo}') WHERE RNK <= 1
-    }
-    public static String querySQL(HashMap<String, Object> params)throws SQLException, Exception {
-        String Host = (String) params.get("HOST");
-        String Username = (String) params.get("USERNAME");
-        String Password = (String) params.get("PASSWORD");
-        String Query = (String) params.get("QUERY");
-        String ColumnName = (String) params.get("COLUMN NAME");
-        String returnNumRecords = (String) params.get("EXPECTED RESULT");
-        String ColumnData = null;
-        Class.forName("oracle.jdbc.driver.OracleDriver");
-        Connection con = null;
-        try {
-            con = DriverManager.getConnection(Host, Username, Password);
-            Statement stmt = con.createStatement();
-            System.out.println("Given Query : " + Query);
-            ResultSet rs = stmt.executeQuery(Query);
-            int no_of_rows = 0;
-            if (!rs.equals(null)) {
-                while (rs.next()) {
-                    ColumnData = rs.getString(ColumnName);
-                    no_of_rows++;
-                    System.out.println("Column Name is :" +  ColumnName);
-                    System.out.println("Column Name Data is :" +  ColumnData);
-                }
-                System.out.println("No. of Rows in DB are : " + no_of_rows);
-            }
-            if (no_of_rows > 1) {
-                System.out.println("As Multiple rows are found data is compared with last row");
-            }else if (no_of_rows ==0){
-                throw new Exception("No Records found , Check query by manually runing in the db : " + Query);
-            }
-        } catch (Exception ex) {
-            throw new Exception("ERROR : " + ex.getMessage());
-        } finally {
-            if(!(con==null))
-                con.close();
-        }
-        Assert.assertEquals(ColumnData, returnNumRecords, "Data is NOT Matching");
-
-        return ColumnData;
-      //  System.out.println(ColumnData);
-    }
+ // write your code here
 
 }
